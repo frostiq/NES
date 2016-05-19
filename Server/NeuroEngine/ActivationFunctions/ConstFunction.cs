afbb59d1aacdfa41df5441c9129ad11b39cc3c8e
a.bazhanau <alex.bazhanau@adform.com>
2016-05-19 23:11:05 +0300

    const func added

---
+++ Server/NeuroEngine/ActivationFunctions/ConstFunction.cs
@@ -0,0 +1,14 @@
+﻿namespace NeuroEngine.ActivationFunctions
+{
+    public class ConstFunction : IActivationFunction
+    {
+        private readonly double _output;
+
+        public ConstFunction(double output)
+        {
+            _output = output;
+        }
+
+        public double Apply(double input) => _output;
+    }
+}
